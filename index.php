c115b4f3edaa5de9aa32db261a894454da66ed71
parmar-viral <parmarviral8324@gmail.com>
2023-09-02 17:56:26 -0700

    css and js link

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Saustudy</title>
+    <?php include 'css.php'; ?>
 </head>
 <body>
-    
+<h1 class="btn btn-primary">saustudy</h1> 
+
+<?php include 'js.php'; ?>
 </body>
 </html>
---
+++ js.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+  </head>
+  <body>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
