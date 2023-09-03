07f27cce07051012f825f1fc3ffc72cd63964ea1
shakti-vanani <vananishaktisir@gmail.com>
2023-09-03 16:18:05 +0530

    link

--- js.php
+++ js.php
@@ -7,6 +7,6 @@
   </head>
   <body>
    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
