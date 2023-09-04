03248cdea2af14e3e5df256309264f5ebfcebe7c
parmar-viral <parmarviral8324@gmail.com>
2023-09-04 14:02:50 -0700

    btn update

--- view.php
+++ view.php
@@ -42,10 +42,10 @@ $obj1=new user();
 				<?php  echo $row["username"];  ?>
 			</td>
 			<td>
-					<a href="/saustudy/update.php?id=<?php echo $row["id"]; ?>"><span class="button">EDIT</span></a>
+					<a href="/saustudy/update.php?id=<?php echo $row["id"]; ?>"><span class="btn btn-primary">EDIT</span></a>
 		 </td>
 		 <td>		
-					<a href="/saustudy/delete.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('are you sure to delete')" ><span class="bg bg-danger">DELETE</span></a>
+					<a href="/saustudy/delete.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('are you sure to delete')" ><span class="btn btn-danger">DELETE</span></a>
                 </td>
 		</tr>
 		<?php	}		
