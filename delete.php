929facc2677dae5b189d4a75fdad427c2bc5841a
parmar-viral <parmarviral8324@gmail.com>
2023-09-06 19:14:55 -0700

    delete

--- delete.php
+++ delete.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>delete</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>delete</title>
     <?php include 'css.php'; ?>
 </head>
 <body>
@@ -12,8 +12,10 @@
     ?>
     <form action="main.php" method="POST">
         <input type="number" value="<?php  echo $id ?>" name="id" hidden>
-         <?php echo "delete this id ". $id ?> <button class="btn btn-danger m-3" type="submit" name="delete"  onclick="return confirm('are you sure to delete')">delete</button>
-</form>
-<?php include 'js.php'; ?> 
+        <?php echo "delete this id ". $id ?> <button class="btn btn-danger m-3" type="submit" name="delete"
+            onclick="return confirm('are you sure to delete')">delete</button>
+
+    </form>
+    <?php include 'js.php'; ?>
 </body>
 </html>
