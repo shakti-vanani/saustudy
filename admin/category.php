0a0d50b6abfc7d60caca22a4504ad2621b8d91f1
parmar-viral <parmarviral8324@gmail.com>
2023-09-09 17:10:54 -0700

    category.php update

--- admin/category.php
+++ admin/category.php
@@ -1,6 +1,6 @@
 <?php
 include 'error.php';
-class semester
+class category
 {
     private $conn = '';
     function __construct()
@@ -31,7 +31,7 @@ class semester
     }
     function view()
     {
-        $sql = "SELECT * FROM `category`";
+        $sql = "SELECT category_id,course,semester,category FROM category INNER JOIN courses USING(course_id) INNER JOIN semesters USING(semester_id)";
         $res = mysqli_query($this->db, $sql);
         return $res;
     }
@@ -48,7 +48,7 @@ class semester
     return $res;    
     }
 }
-$obj = new semester();
+$obj = new category();
 if (isset($_POST['submit'])) {    
     $course_id=$_POST['course_id'];
     //echo $course_id;
@@ -84,140 +84,148 @@ if (isset($_POST['update'])) {
     }
 }
 
-//$obj1=new semster();
+//$obj1=new category();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin|Login</title>
     <?php include 'css.php'; ?>
 </head>
-<body>     
+
+<body>
     <?php include 'menu.php'; ?>
-    
+
     <div class="main-container">
-			<div class="xs-pd-20-10 pd-ltr-20">
-				<div class="page-header">
-					<div class="row">
-						<div class="col-md-6 col-sm-12">
-							<div class="title">
-								<h4>Semesters</h4>
-							</div>
-							<nav aria-label="breadcrumb" role="navigation">
-								<ol class="breadcrumb">
-									<li class="breadcrumb-item"><a href="index.php">Home</a></li>
-									<li class="breadcrumb-item active" aria-current="page">
-                                    Semesters
-									</li>
-								</ol>
-							</nav>
-						</div>
-					</div>
-				</div>
-                <div class="row xs-pd-10-10 pd-ltr-20 mb-10 ">
-                <div class="col-md-12 col-sm-12 card-box">
-                <form action="" method="POST">
-                <div class="form-group row pd-10">
-		<label class="col-sm-12 col-md-2 col-form-label"><div class="title">
-								<h4>Course Select</h4>
-							</div></label></label>
-		<div class="col-sm-12 col-md-8">
-        <?php
-
-$data = $obj->courseview();                           
-?>
-			<select class="custom-select col-12" name="course_id">
-				<option selected="">Choose...</option>
-				<?php
-                                       while( $row = mysqli_fetch_assoc($data))
-                                       {
-                                        ?>
-                                    <option value="<?php echo $row['course_id']; ?>"><?php echo $row["course"]; ?></option>
-                                    <?php } ?>
-			</select>
-		</div>
-	</div>
-    <div class="form-group row pd-10">
-		<label class="col-sm-12 col-md-2 col-form-label"><div class="title">
-								<h4>Semester Select</h4>
-							</div></label></label>
-		<div class="col-sm-12 col-md-8">
-        <?php
-
-$data = $obj->semesterview();                           
-?>
-			<select class="custom-select col-12" name="semester_id">
-				<option selected="">Choose...</option>
-				<?php
-                                       while( $row = mysqli_fetch_assoc($data))
-                                       {
-                                        ?>
-                                    <option value="<?php echo $row['semester_id']; ?>"><?php echo $row["semester"]; ?></option>
-                                    <?php } ?>
-			</select>
-		</div>
-	</div>
-                <div class="form-group row pd-10">
-                    
-                <label class="col-sm-12 col-md-2 col-form-label">
-                    <div class="title">
-								<h4>Category Name</h4>
-							</div></label>
-                <div class="col-sm-12 col-md-8">
-                    <input class="form-control" type="text" placeholder="Add New Category" name="category">
-                </div>
-                <div class="col-sm-12 col-md-2">
-                  <button type="submit" name="submit" class="btn btn-success">submit</button>
+        <div class="xs-pd-20-10 pd-ltr-20">
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="title">
+                            <h4>Semesters</h4>
+                        </div>
+                        <nav aria-label="breadcrumb" role="navigation">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    category
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
                 </div>
             </div>
-                </form>
-                </div>                       
-				</div>
-				<div class="row xs-pd-20-10 pd-ltr-20 mb-20">
+            <div class="row xs-pd-10-10 pd-ltr-20 mb-10 ">
+                <div class="col-md-12 col-sm-12 card-box">
+                    <form action="" method="POST">
+                        <div class="form-group row pd-10">
+                            <label class="col-sm-12 col-md-2 col-form-label">
+                                <div class="title">
+                                    <h4>Course Select</h4>
+                                </div>
+                            </label></label>
+                            <div class="col-sm-12 col-md-8">
+                                <?php
+
+$data = $obj->courseview();                        
+?>
+                                <select class="custom-select col-12" name="course_id">
+                                    <option selected="">Choose...</option>
+                                    <?php
+                                       while( $row = mysqli_fetch_assoc($data))
+                                       {
+                                        ?>
+                                    <option value="<?php echo $row['course_id']; ?>"><?php echo $row["course"]; ?>
+                                    </option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row pd-10">
+                            <label class="col-sm-12 col-md-2 col-form-label">
+                                <div class="title">
+                                    <h4>Semester Select</h4>
+                                </div>
+                            </label></label>
+                            <div class="col-sm-12 col-md-8">
+                                <?php
+         
+$data = $obj->semesterview();                           
+?>
+                                <select class="custom-select col-12" name="semester_id">
+                                    <option selected="">Choose...</option>
+                                    <?php
+                                       while( $row = mysqli_fetch_assoc($data))
+                                       {
+                                        ?>
+                                    <option value="<?php echo $row['semester_id']; ?>"><?php echo $row["semester"]; ?>
+                                    </option>
+                                    <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row pd-10">
+
+                            <label class="col-sm-12 col-md-2 col-form-label">
+                                <div class="title">
+                                    <h4>Category Name</h4>
+                                </div>
+                            </label>
+                            <div class="col-sm-12 col-md-8">
+                                <input class="form-control" type="text" placeholder="Add New Category" name="category">
+                            </div>
+                            <div class="col-sm-12 col-md-2">
+                                <button type="submit" name="submit" class="btn btn-success">submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row xs-pd-20-10 pd-ltr-20 mb-20">
                 <div class="col-md-12 col-sm-12   card-box">
                     <table class="table table-striped">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Courses</th>
-                        <th scope="col">Semesters</th>
-                        <th scope="col">Category</th>
-                        <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Courses</th>
+                                <th scope="col">Semesters</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
                             $data = $obj->view();
                             while ($row = mysqli_fetch_assoc($data)) {
                                 ?>
-                        <tr>
-                        <th scope="row"><?php echo $row['category_id']; ?></th>
-                        <td><?php echo $row['course_id']; ?></td>
-                        <td><?php echo $row['semester_id']; ?></td>
-                        <td><?php echo $row['category']; ?></td>
-                        <td>
-                            <a href="" class="text-primary">
+                            <tr>
+                                <th scope="row"><?php echo $row['category_id']; ?></th>
+                                <td><?php echo $row['course']; ?></td>
+                                <td><?php echo $row['semester']; ?></td>
+                                <td><?php echo $row['category']; ?></td>
+                                <td>
+                                <a href="" class="text-primary">
                             <span class="micon fa fa-edit">   Edit</span>
                             </a>   |   <a href="" class="text-danger"><span class="micon ion-trash-a">   Delete</span></a>
-                        </td>
-                        </tr>
-                        <?php } ?>
-                    </tbody>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
                     </table>
-</div>                       
-				</div>
-				
-				<div class="footer-wrap pd-20 mb-20 card-box">
-					Saustudy Project By:[1] Shakti Vanani [2] Viral Parmar
-					<a href="https://github.com/shakti-vanani" target="_blank"
-						>Shakti Vanani</a
-					>
-				</div>
-			</div>
-		</div>
+                </div>
+            </div>
+
+            <div class="footer-wrap pd-20 mb-20 card-box">
+                Saustudy Project By:[1] Shakti Vanani [2] Viral Parmar
+                <a href="https://github.com/shakti-vanani" target="_blank">Shakti Vanani</a>
+            </div>
+        </div>
+    </div>
 
     <?php include 'js.php'; ?>
 </body>
+
 </html>
