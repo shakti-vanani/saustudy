a2284821baa4831bb2f0aefea219282549d7d92f
shakti-vanani <vananishaktisir@gmail.com>
2023-09-13 17:34:25 +0530

    create topic table

---
+++ admin/database/create_topic_table.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "saustudy";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+//sql to topics table
+$sql = "CREATE TABLE topics
+(
+    topic_id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    course_id  INT(2) NOT NULL,
+    semester_id INT(2) NOT NULL,
+    category_id INT(2) NOT NULL,
+    matirial_id INT(2) NOT NULL,
+    topic VARCHAR(30) NOT NULL,
+    topic_detail VARCHAR(30) NOT NULL,
+    create_at DATE NOT NULL,
+    update_at DATE NOT NULL
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table topics created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+?>
