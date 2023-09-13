a5d291c501842075b49ec954161517553f5a69c8
parmar-viral <parmarviral8324@gmail.com>
2023-09-12 21:09:16 -0700

    topic table create

--- admin/database/create_table.php
+++ admin/database/create_table.php
@@ -67,6 +67,25 @@ if (mysqli_query($conn, $sql)) {
 } else {
   echo "Error creating table: " . mysqli_error($conn);
 }
+//sql to topics table
+$sql = "CREATE TABLE topics
+(
+    topic_id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    course_id  INT(2) NOT NULL,
+    semester_id INT(2) NOT NULL,
+    category_id INT(2) NOT NULL,
+    matirial_id INT(2) NOT NULL,
+    topic VARCHAR(30) NOT NULL,
+    topic_detail VARCHAR(30) NOT NULL,
+    create_at DATE NOT NULL,
+    update_at DATE NOT NULL
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table topics created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
 
 
 mysqli_close($conn);
