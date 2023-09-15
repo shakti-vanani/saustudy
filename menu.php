da9f7cea4fd4c0bbbb46afb709a461c31cc84d30
shakti-vanani <vananishaktisir@gmail.com>
2023-09-15 20:11:47 +0530

    update data

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar viral-card  m-2 navbar-expand-lg ">
 	<div class="container-fluid">
-		<a class="navbar-brand viral-card p-2" href="#"> Saustudy </a>
+		<a class="navbar-brand  p-2" href="#"> Saustudy </a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link " href="#">Home</a>
+					<a class="nav-link  " href="#">Home</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link " href="#">Courses</a>
