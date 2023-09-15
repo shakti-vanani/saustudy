0088dc86f299daf659b490e004dc2e66ca692eb4
shakti-vanani <vananishaktisir@gmail.com>
2023-09-15 20:01:34 +0530

    update

--- index.php
+++ index.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Saustudy</title>
 	<?php include 'css.php'; ?>
 </head>
+
 <body>
-<?php include 'menu.php'; ?>
+	<?php include 'menu.php'; ?>
 
-<div class="container">
-<div class="row viral-card mt-3">
-                    <div class="col viral-card-1 m-1">
-                        <div>
-                            <canvas id="myData"></canvas>
-                        </div>
-                    </div>
+	<div class="container">
+		<div class="row viral-card mt-3">
+			<div class="col  m-1">
+				devloper
+			</div>
+		</div>
+	</div>
 
-                    <div class="col-3 m-1">
-                        <div>
-                            <canvas id="myData2"></canvas>
-                        </div>
-                    </div>
-                </div>
-</div>
-
-<?php include 'js.php'; ?>
+	<?php include 'js.php'; ?>
 </body>
+
 </html>
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
-<nav class="navbar viral-card m-2 navbar-expand-lg ">
+<nav class="navbar viral-card  m-2 navbar-expand-lg ">
 	<div class="container-fluid">
-		<a class="navbar-brand viral-card-3 p-2" href="#"> Saustudy </a>
+		<a class="navbar-brand viral-card p-2" href="#"> Saustudy </a>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
