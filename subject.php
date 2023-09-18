76ecd2a3b935e8f006dfe334c7f601442f8b560a
shakti-vanani <vananishaktisir@gmail.com>
2023-09-18 22:26:01 +0530

    subject with category update

--- subject.php
+++ subject.php
@@ -15,41 +15,41 @@
     <div class="container">
         <div class="row mt-3">
 
-			<?php
-			 $id=$_POST['semester_id'];
-             //echo $id;
-               $sql="SELECT * FROM subjects WHERE semester_id='$id'";      
-               $res = mysqli_query($conn, $sql);
-               
-               while ($row = mysqli_fetch_assoc($res)) {
-				?>
-				<div class="col  m-1">
-					<div class="card viral-card m-1 text-center p-1">
-						<h4>
-                        <?php echo $row["subject_name"];
-                            $subject=$row["subject_id"];
-                        ?>
-						</h4>
-						<form action="category.php" method="POST">
-                       <?php 
-                       
-                       $sql="SELECT * FROM category WHERE subject_id='$subject'";      
-               $res = mysqli_query($conn, $sql);
-               
-               while ($row = mysqli_fetch_assoc($res)) {
-				?> 
-							<button class="btn viral-card-2 m-3" type="submit" name="course_id"
-								value="<?php echo $row["category_id"]; ?>"><?php echo $row["category"]; ?></button>
-                                <?php }
-            
-             ?>
-						</form>
-					</div>
+            <?php
+            $id = $_POST['semester_id'];
+            //echo $id;
+            $sql = "SELECT * FROM subjects WHERE semester_id='$id'";
+            $res = mysqli_query($conn, $sql);
 
-				</div>
-			<?php }
-            
-             ?>
+            while ($row = mysqli_fetch_assoc($res)) {
+                ?>
+                <div class="col  m-1">
+                    <div class="card viral-card m-1 text-center p-1">
+                        <h4>
+                            <?php echo $row["subject_name"];
+                            $subject = $row["subject_id"];
+                            ?>
+                        </h4>
+                        <form action="category.php" method="POST">
+                            <?php
+
+                            $sql = "SELECT * FROM category WHERE subject_id='$subject'";
+                            $res = mysqli_query($conn, $sql);
+
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                ?>
+                                <button class="btn viral-card-2 m-3" type="submit" name="course_id"
+                                    value="<?php echo $row["category_id"]; ?>"><?php echo $row["category"]; ?></button>
+                            <?php }
+
+                            ?>
+                        </form>
+                    </div>
+
+                </div>
+            <?php }
+
+            ?>
         </div>
     </div>
     </div>
