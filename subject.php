f6f83b33e813338fe5770cf405efb4bad23f63e0
shakti-vanani <vananishaktisir@gmail.com>
2023-09-21 22:06:57 +0530

    subject update

--- subject.php
+++ subject.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'admin/error.php';
 session_start();
 // Include database connection file
@@ -7,75 +7,74 @@ if (!isset($_SESSION['ID'])) {
     header("Location:index.php");
     exit();
 }
-if(2==$_SESSION['ROLE']){
-?>
-<!DOCTYPE html>
-<html lang="en">
+if (2 == $_SESSION['ROLE']) {
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saustudy</title>
-    <?php include 'css.php'; ?>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Saustudy</title>
+        <?php include 'css.php'; ?>
+    </head>
 
-<body>
-    <?php include 'menu.php'; ?>
+    <body>
+        <?php include 'menu.php'; ?>
 
-    <div class="container">
-    <div class="row mt-3">
-			<?php include 'slider.php'; ?>
-		</div>
-        <div class="row mt-3">
+        <div class="container">
+            <div class="row mt-3">
+                <?php include 'slider.php'; ?>
+            </div>
+            <div class="row mt-3">
 
-            <?php
-            $id = $_POST['semester_id'];
-            //echo $id;
-            $sql = "SELECT * FROM subjects WHERE semester_id='$id'";
-            $res = mysqli_query($conn, $sql);
+                <?php
+                $id = $_POST['semester_id'];
+                //echo $id;
+                $sql = "SELECT * FROM subjects WHERE semester_id='$id'";
+                $res = mysqli_query($conn, $sql);
 
-            while ($row = mysqli_fetch_assoc($res)) {
-                ?>
-                <div class="col-md-4 col-sm-12  m-1">
-                    <div class="card viral-card m-1 text-center p-1">
-                        <h4>
-                            <?php echo $row["subject_name"];
-                            $subject = $row["subject_id"];
-                            ?>
-                        </h4>
-                        <form action="topics_list.php" method="POST">
-                            <?php
-
-                            $sql = "SELECT * FROM category WHERE subject_id='$subject'";
-                            $res = mysqli_query($conn, $sql);
-
-                            while ($row = mysqli_fetch_assoc($res)) {
+                while ($row = mysqli_fetch_assoc($res)) {
+                    ?>
+                    <div class="col-md-4 col-sm-12  m-1">
+                        <div class="card viral-card m-1 text-center p-1">
+                            <h4>
+                                <?php echo $row["subject_name"];
+                                $subject = $row["subject_id"];
                                 ?>
-                                <button class="btn viral-card-2 m-3" type="submit" name="category_id"
-                                    value="<?php echo $row["category_id"]; ?>"><?php echo $row["category"]; ?></button>
-                            <?php }
+                            </h4>
+                            <div>
+                                <form action="topics_list.php" method="POST">
+                                    <?php
 
-                            ?>
-                        </form>
+                                    $csql = "SELECT * FROM category WHERE subject_id='$subject'";
+                                    $cres = mysqli_query($conn, $csql);
+
+                                    while ($crow = mysqli_fetch_assoc($cres)) { ?>
+                                        <button class="btn viral-card-2 m-3" type="submit" name="category_id"
+                                            value="<?php echo $crow["category_id"]; ?>">
+                                            <?php echo $crow["category"]; ?>
+                                        </button>
+                                    <?php } ?>
+                                </form>
+                            </div>
+                        </div>
+
                     </div>
-
-                </div>
-            <?php }
-
-            ?>
+                <?php } ?>
+            </div>
         </div>
-    </div>
-    
-    <?php include 'footer.php'; ?>
 
-    <?php include 'js.php'; ?>
-</body>
+        <?php include 'footer.php'; ?>
 
-</html>
+        <?php include 'js.php'; ?>
+    </body>
 
-<?php }else{
-            session_destroy();
-            header("Location:index.php");
-        }
-        
-        ?>
+    </html>
+
+<?php } else {
+    session_destroy();
+    header("Location:index.php");
+}
+
+?>
