d3d84d06dc23fa295b8bec88ef4b9af36b30d554
shakti-vanani <vananishaktisir@gmail.com>
2023-09-23 18:11:18 +0530

    update delete

--- admin2/courses.php
+++ admin2/courses.php
@@ -27,57 +27,69 @@ if (0 == $_SESSION['ROLE']) {
         <div class="container  ">
 
             <div class="row p-2 mt-1">
-                
-                    <div class=" viral-card text-center">
-                        <form class="mt-3" action="" method="POST">
 
-                            <div class="input-group mb-3">
-                                <span class="input-group-text  viral-card-2 col-2" id="course"><h5><i
-                                        class="bi bi-journal"></i>Course</h5></span>
-                                <input type="text" name="course" class="viral-card-1  p-2 col-8" placeholder="Add New Course">
-                           
-                                <button type="submit" name="submit" class="btn viral-card-2 p-2 col-2">submit</button>
-                            </div>
-                        </form>
-                    </div>
+                <div class=" viral-card text-center">
+                    <form class="mt-3" action="" method="POST">
 
-            </div> 
-                <?php /*
-            $data = $obj->view();
-            while ($row = mysqli_fetch_assoc($data)){
-               ?>
-               <div class="col-lg-4 col-md-4 col-sm-12 ">
-                   <div class="card viral-card m-1 text-center p-1">
-                       <h4>
-                           <?php echo $row["username"]; ?>
-                       </h4>
-                       <div class="">
-                           <p><?php echo $row["fname"]; ?></p>
-                           <p><?php echo $row["lname"]; ?></p>
-                           <p><?php echo $row["email"]; ?></p>
-                       </div>
-                       
-                   </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text  viral-card-2 col-2" id="course">
+                                <h5><i class="bi bi-journal"></i>Course</h5>
+                            </span>
+                            <input type="text" name="course" class="viral-card-1  p-2 col-8" placeholder="Add New Course">
 
-               </div>
-           <?php  } */
-                ?>
-            
+                            <button type="submit" name="submit" class="btn viral-card-2 p-2 col-2">submit</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+            <?php /*
+           $data = $obj->view();
+           while ($row = mysqli_fetch_assoc($data)){
+              ?>
+              <div class="col-lg-4 col-md-4 col-sm-12 ">
+                  <div class="card viral-card m-1 text-center p-1">
+                      <h4>
+                          <?php echo $row["username"]; ?>
+                      </h4>
+                      <div class="">
+                          <p><?php echo $row["fname"]; ?></p>
+                          <p><?php echo $row["lname"]; ?></p>
+                          <p><?php echo $row["email"]; ?></p>
+                      </div>
+                      
+                  </div>
+
+              </div>
+          <?php  } */
+            ?>
+
             <div class="row  p-2 mt-1">
                 <div class="row viral-card mt-2 p-1">
-                <div class="col-1">#</div>
-                <div class="col">Course Name</div>
-                <div class="col">Action</div>
+                    <div class="col-1">#</div>
+                    <div class="col">Course Name</div>
+                    <div class="col">Action</div>
                 </div>
-               <?php  $data = $obj->view();
-                            while ($row = mysqli_fetch_assoc($data)) {
-                                ?>
-                <div class="row viral-card mt-2 p-1">
-                <div class="col-1"> <?php echo $row["course_id"]; ?></div>
-                <div class="col"> <?php echo $row["course"]; ?></div>
-                <div class="col"></div>
-                </div>
-            <?php } ?>
+                <?php $data = $obj->view();
+                while ($row = mysqli_fetch_assoc($data)) {
+                    ?>
+                    <div class="row viral-card mt-2 p-1">
+                        <div class="col-1">
+                            <?php echo $row["course_id"]; ?>
+                        </div>
+                        <div class="col">
+                            <?php echo $row["course"]; ?>
+                        </div>
+                        <div class="col">
+                            
+                            <form action="" method="POST">
+                                            <input type="number" value="<?php echo $row["course_id"]; ?>" name="id" hidden>
+                                            <button class="btn viral-card-2" type="submit" name="delete"
+                                                onclick="return confirm('are you sure to delete')">delete</button>
+                            </form>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
 
 
