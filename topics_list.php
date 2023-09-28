85514235f84329cde95474c3495d0005d9205c9f
shakti-vanani <vananishaktisir@gmail.com>
2023-09-28 22:12:45 +0530

    update topic

--- topics_list.php
+++ topics_list.php
@@ -25,17 +25,48 @@ if (2 == $_SESSION['ROLE']) {
         <div class="container">
             
             <div class="row mt-3">
-
+            <div class="card col-12 viral-card"><h3>PDF</h3></div>
                 <?php
                 $id = $_POST['chapter_id'];
-                //echo $id;
-                $sql = "SELECT * FROM topics WHERE chapter_id='$id'";
+                $tid=1;
+                $sql = "SELECT * FROM topics WHERE chapter_id='$id' AND topic_category='$tid'";
                 $res = mysqli_query($conn, $sql);
-
+                
                 while ($row = mysqli_fetch_assoc($res)) {
                     ?>
+
                     <div class="col-md-4 col-sm-12 ">
+                    
                         <div class="card viral-card m-1 text-center p-1">
+                           
+                            <h4>
+                                <?php echo $row["topic"];
+
+                                $subject = $row["topic_id"];
+                                ?>
+                            </h4>
+                        </div>
+
+                    </div>
+                <?php }
+
+                ?>
+            </div>
+            <div class="row mt-3">
+            <div class="card col-12 viral-card"><h3>Video</h3></div>
+                <?php
+                $id = $_POST['chapter_id'];
+                $tid=2;
+                $sql = "SELECT * FROM topics WHERE chapter_id='$id' AND topic_category='$tid'";
+                $res = mysqli_query($conn, $sql);
+                
+                while ($row = mysqli_fetch_assoc($res)) {
+                    ?>
+
+                    <div class="col-md-4 col-sm-12 ">
+                    
+                        <div class="card viral-card m-1 text-center p-1">
+                           
                             <h4>
                                 <?php echo $row["topic"];
 
@@ -46,11 +77,40 @@ if (2 == $_SESSION['ROLE']) {
                             <div class="embed-responsive ">
                             <iframe class="embed-responsive-item" width="400" height="315"  src="<?php echo $row['topic_link']; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;  encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
                            </div>
-                            <form action="topic_details.php" method="POST">
+                           
+                        </div>
 
-                                <button class="btn viral-card-2 m-3" type="submit" name="topic_id"
-                                    value="<?php echo $row["topic_id"]; ?>">View</button>
-                            </form>
+                    </div>
+                <?php }
+
+                ?>
+            </div>
+            <div class="row mt-3">
+            <div class="card col-12 viral-card"><h3>Extra</h3></div>
+                <?php
+                $id = $_POST['chapter_id'];
+                $tid=3;
+                $sql = "SELECT * FROM topics WHERE chapter_id='$id' AND topic_category='$tid'";
+                $res = mysqli_query($conn, $sql);
+                
+                while ($row = mysqli_fetch_assoc($res)) {
+                    ?>
+
+                    <div class="col-md-4 col-sm-12 ">
+                    
+                        <div class="card viral-card m-1 text-center p-1">
+                           
+                            <h4>
+                                <?php echo $row["topic"];
+
+                                $subject = $row["topic_id"];
+                                ?>
+                            </h4>
+
+                            <div class="embed-responsive ">
+                            <iframe class="embed-responsive-item" width="400" height="315"  src="<?php echo $row['topic_link']; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;  encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
+                           </div>
+                           
                         </div>
 
                     </div>
